951794f30b9663ce55126c2e265ea8a82f33110a
weiyunkj666 <32947593+weiyunkj666@users.noreply.github.com>
2023-04-19 17:10:41 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,23 @@
+export default {
+
+    async fetch(request, env) {
+
+      let url = new URL(request.url);
+
+      if (url.pathname.startsWith('/')) {
+
+        // 修改下方的example.com为自己的节点ip/域名
+
+        url.hostname="cf-nathgc66.9990202.xyz:2053";
+
+        let new_request=new Request(url,request);
+
+        return fetch(new_request);
+
+      }
+
+      return env.ASSETS.fetch(request);
+
+    }
+
+  };
